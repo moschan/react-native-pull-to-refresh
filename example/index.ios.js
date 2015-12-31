8d13284d8b6fc7bac29a2735ead8d35878d46a7d
moschan <kaityo02@gmail.com>
2015-12-31 14:48:33 +0800

    implemented ver0.0.1

--- example/index.ios.js
+++ example/index.ios.js
@@ -17,25 +17,27 @@ var {
 var PullToRefreshProject = React.createClass({
   _refresh: function() {
     return new Promise((resolve) => {
-      setTimeout(()=>{resolve()}, 1000)
+      setTimeout(()=>{resolve()}, 2000)
     });
   },
   render: function() {
     return (
-      <PTRView onRefresh={this._refresh} >
-        <View style={styles.container}>
-          <Text style={styles.welcome}>
-            Welcome to React Native!
-          </Text>
-          <Text style={styles.instructions}>
-            To get started, edit index.ios.js
-          </Text>
-          <Text style={styles.instructions}>
-            Press Cmd+R to reload,{'\n'}
-            Cmd+D or shake for dev menu
-          </Text>
+      <View style={{flex:1}}>
+        <View style={styles.header}>
+          <Text style={styles.headerText}>PullToRefreshView Demo</Text>
         </View>
-      </PTRView>
+        <PTRView
+          style={{backgroundColor:'#F5FCFF'}}
+          onRefresh={this._refresh}
+        >
+          <View style={styles.container}>
+            <Text style={styles.welcome}>
+              Let's Pull!
+            </Text>
+          </View>
+        </PTRView>
+      </View>
+
     );
   },
 });
@@ -45,8 +47,6 @@ var styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
-    // backgroundColor: '#F5FCFF',
-    // backgroundColor: 'rgba(0,0,0,0)',
   },
   welcome: {
     fontSize: 20,
@@ -58,6 +58,19 @@ var styles = StyleSheet.create({
     color: '#333333',
     marginBottom: 5,
   },
+  header: {
+    height: 60,
+    borderColor: '#f9f9f9',
+    backgroundColor: '#2196F3',
+    borderBottomWidth: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  headerText: {
+    color: '#fff',
+    fontSize: 20,
+    lineHeight: 40,
+  }
 });
 
 AppRegistry.registerComponent('PullToRefreshProject', () => PullToRefreshProject);
