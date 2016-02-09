f24cc7f11f731b10c7127ad90a847c0f906f232d
moschan <kaityo02@gmail.com>
2016-02-09 13:25:38 +0800

    adapt to android

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
-import PTRView from './lib/PullToRefreshViewiOS.js'
+import { Platform } from 'react-native'
+import PTRViewiOS from './lib/PullToRefreshViewiOS.js'
+import PTRViewAndroid from './lib/PullToRefreshViewAndroid.js'
+
+var PTRView = false
+
+if (Platform.OS === 'android') {
+  PTRView = PTRViewAndroid
+} else {
+  PTRView = PTRViewiOS
+}
+
 module.exports = PTRView
 
