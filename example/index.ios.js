b41dde50756dbcbf3f4d7f2821e2e180bcb0cf69
moschan <kota.frsw@gmail.com>
2017-07-16 11:51:08 +0800

    update example

--- example/index.ios.js
+++ example/index.ios.js
@@ -12,13 +12,26 @@ import {
   View
 } from 'react-native';
 
-// import PTRView from './index.js'
+
 import PTRView from 'react-native-pull-to-refresh'
 
-class PullToRefreshExample extends Component {
+export default class PullToRefreshExample extends Component {
+  constructor(param) {
+    super(param);
+    this.state = {
+      cards: [1, 2, 3]
+    }
+    this._refresh = this._refresh.bind(this);
+
+  }
   _refresh () {
+    // you must return Promise everytime
     return new Promise((resolve) => {
-      setTimeout(()=>{resolve()}, 2000)
+      setTimeout(()=>{
+        // some refresh process should come here
+        this.setState({cards: this.state.cards.concat([this.state.cards.length + 1])})
+        resolve(); 
+      }, 2000)
     })
   }
   render() {
@@ -32,9 +45,12 @@ class PullToRefreshExample extends Component {
           onRefresh={this._refresh}
         >
           <View style={styles.container}>
-            <Text style={styles.welcome}>
-              Pull Me!😸
-            </Text>
+            <Text style={styles.welcome}> Pull Me!😸 </Text>
+            {this.state.cards.map((el, i) => (
+              <View style={styles.card} key={i}>
+                <Text style={styles.card__text}>Card: {el}</Text>
+              </View>
+            ))}
           </View>
         </PTRView>
       </View>
@@ -46,7 +62,7 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     justifyContent: 'center',
-    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
   },
   welcome: {
     fontSize: 20,
@@ -70,6 +86,20 @@ const styles = StyleSheet.create({
     color: '#fff',
     fontSize: 20,
     lineHeight: 40,
+  },
+  card: {
+    flex: 1,
+    borderColor: '#fafafa',
+    backgroundColor: '#2196F3',
+    borderWidth: 2,
+    borderRadius: 3,
+    margin: 5,
+  },
+  card__text: {
+    color: '#fafafa',
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
   }
 });
 
